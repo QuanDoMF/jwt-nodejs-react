42061687ecdf092449afa2e846ddd802b5fb2668
Tquandoo <quantrungdo2401@gmail.com>
2023-10-23 15:30:20 +0700

    create new user

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const initWebRoutes = (app) => {
   router.get("/", homeController.handleHelloWord);
   router.get("/user", homeController.handleUserPage);
-
+  router.post("/user/create-user", homeController.handleCreateNewUser);
   return app.use("/", router);
 };
 export default initWebRoutes;
--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
-
+import bodyParser from "body-parser";
 const app = express();
 // config view engine
 configViewEngine(app);
-// init
+//config body parse
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// init web routes
 initWebRoutes(app);
 
 const PORT = process.env.PORT || 8080;
