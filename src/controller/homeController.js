3eeeaed33e12809fc97b4e804f3c19015aea2484
Tquandoo <quantrungdo2401@gmail.com>
2023-10-30 14:50:30 +0700

    hash user password

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,5 @@
-import mysql from "mysql2";
-
 // create the connection to database
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "jwt",
-});
-
+import userService from "../service/userService";
 const handleHelloWord = (req, res) => {
   return res.render("home.ejs");
 };
@@ -19,15 +12,8 @@ const handleCreateNewUser = (req, res) => {
   let password = req.body.password;
   let username = req.body.username;
 
-  connection.query(
-    " INSERT INTO users( email, password, username) VALUES (?, ?, ?)",
-    [email, password, username],
-    function (err, results, fields) {
-      if (err) {
-        console.log(err);
-      }
-    }
-  );
+  userService.createNewUser(email, password, username);
+  userService.getUserList();
   return res.send("handleCreateNewUser");
 };
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from "body-parser";
+
 const app = express();
 // config view engine
 configViewEngine(app);
